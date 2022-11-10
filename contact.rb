1a5ef1a12ba0cbcde807b6bf4d04f40cfff685a5
Henry Doan <henrydoan96@gmail.com>
2022-11-09 21:26:07 -0700

    sudo code

---
+++ contact.rb
@@ -0,0 +1,28 @@
+# sudo code, comment out the process, breaking up our objects 
+# Basic Objectives
+
+# Build a contact list
+  # this is where we are listing out contacts and their info like phone email etc.
+  # like the one on the phone 
+# Create a menu that allows a user to select 1) Create a contact 2) View All Contacts 3) Exit
+  # store all of our contacts 
+  # datatype of what the contact is, what it has 
+  # 1) Create a contact 
+# inputs and outs of grabbing each value 
+#  ex what is the new contact name 
+#  ex what is the new contact phone number 
+    # save the user input to a variable , need a variable for each attr
+    # create a new contact with the variables in a object add to our collection
+  # 2) View All Contacts 
+    # go through the collection one by one 
+    # display them in a reable way 
+  # 3) Exit
+    # say a good msg 
+    # exit the program 
+# The menu should loop until the user exits
+  # loop 
+  # recursion, calling a method within itself 
+
+  # a way to store all contacts 
+
+  # what does a contact have, attr? 
